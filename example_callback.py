fe84a398a7a469df40ccef00359fd0ccb64c49df
PASUNX <34183912+PASUNX@users.noreply.github.com>
2020-03-11 05:22:21 +0700

    Create example_callback.py

---
+++ example_callback.py
@@ -0,0 +1,5 @@
+def sendToChat(client, to, header):
+    newqr = NewQRLogin(lambda output: client.sendMessage(to, output))
+    method = newqr.loginQRWithWebPinCode
+    token, cert = newqr.parseLogin(method(header))
+    client.sendMessage(to, "TOKEN: %s\nCERT: %s" % (token, cert))
